97108238d0a343d444ee37168bf5067b445ad2de
Syaifudin Alkatiri <sfnalkatiri@gmail.com>
2021-05-02 10:49:30 +0700

    try catch handle error

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 //import mongoose from "mongoose";
 const mongoose = require("mongoose");
@@ -24,6 +26,15 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
+app.use(
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+		cookie: { maxAge: 60000 },
+	})
+);
+app.use(flash());
 
 app.use(logger("dev"));
 app.use(express.json());
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,30 +5,62 @@ module.exports = {
 		res.render("admin/dashboard/view", { title: "Dashboard" });
 	},
 	viewCategory: async (req, res) => {
-		const categories = await Category.find({});
-		res.render("admin/category/view", {
-			title: "Category",
-			categories: categories,
-		});
+		try {
+			const categories = await Category.find({});
+			const alertMessage = req.flash("alertMessage");
+			const alertStatus = req.flash("alertStatus");
+			const alert = { alertMessage, alertStatus };
+			res.render("admin/category/view", {
+				title: "Category",
+				categories: categories,
+				alert,
+			});
+		} catch (error) {
+			res.redirect("/admin/category");
+		}
 	},
 	addCategory: async (req, res) => {
-		const { name } = req.body;
-		// console.log(name);
-		Category.create({ name });
-		res.redirect("/admin/category");
+		try {
+			const { name } = req.body;
+			// console.log(name);
+			Category.create({ name });
+			req.flash("alertMessage", "Berhasil ditambahkan");
+			req.flash("alertStatus", "success");
+			res.redirect("/admin/category");
+		} catch (error) {
+			req.flash("alertMessage", "Gagal ditambahkan");
+			req.flash("alertStatus", "danger");
+			res.redirect("/admin/category");
+		}
 	},
 	editCategory: async (req, res) => {
-		const { id, name } = req.body;
-		const category = await Category.findOne({ _id: id });
-		category.name = name;
-		await category.save();
-		res.redirect("/admin/category");
+		try {
+			const { id, name } = req.body;
+			const category = await Category.findOne({ _id: id });
+			category.name = name;
+			await category.save();
+			req.flash("alertMessage", "Berhasil diupdate");
+			req.flash("alertStatus", "success");
+			res.redirect("/admin/category");
+		} catch (error) {
+			req.flash("alertMessage", "Gagal diupdate");
+			req.flash("alertStatus", "danger");
+			res.redirect("/admin/category");
+		}
 	},
 	deleteCategory: async (req, res) => {
-		const { id } = req.params;
-		const category = await Category.findOne({ _id: id });
-		await category.remove();
-		res.redirect("/admin/category");
+		try {
+			const { id } = req.params;
+			const category = await Category.findOne({ _id: id });
+			await category.remove();
+			req.flash("alertMessage", "Berhasil dihapus");
+			req.flash("alertStatus", "success");
+			res.redirect("/admin/category");
+		} catch (error) {
+			req.flash("alertMessage", "Gagal diupdate");
+			req.flash("alertStatus", "danger");
+			res.redirect("/admin/category");
+		}
 	},
 	viewBank: (req, res) => {
 		res.render("admin/bank/view", { title: "Bank" });
